343b003d96e3c7944df2cf6002d341a74f38c90b
shin <nhathanh024@gmail.com>
2025-06-25 14:40:22 +0700

    init code

---
+++ src/wp-content/themes/zippy-elementor-child/includes/zippy_custom.php
@@ -0,0 +1,14 @@
+<?php
+add_action('wp_enqueue_scripts', 'shin_scripts');
+
+function shin_scripts()
+{
+    $version = time();
+
+    wp_enqueue_style('main-style-css', THEME_URL . '-child' . '/assets/dist/css/main.min.css', array(), $version, 'all');
+
+    wp_enqueue_script('main-scripts-js', THEME_URL . '-child' . '/assets/dist/js/main.min.js', array('jquery'), $version, true);
+    wp_enqueue_script('sweet-alert2-js', THEME_URL . '-child' . '/assets/lib/sweetalert/sweetalert2.all.min.js', [], $version, true);
+
+    wp_enqueue_style('sweet-alert2-css', THEME_URL . '-child' . '/assets/lib/sweetalert/sweetalert2.min.css', [], $version);
+}
